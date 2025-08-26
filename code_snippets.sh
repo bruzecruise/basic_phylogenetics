ee488cb78dc99ea8cb0096430140eb5802375ce7
Dan Bruzzese <31770882+bruzecruise@users.noreply.github.com>
2025-08-26 11:40:34 -0400

    Update code_snippets.sh

--- code_snippets.sh
+++ code_snippets.sh
@@ -6,3 +6,13 @@ cat #view file content on the screen
 cat file1 file2 file3 file4 > combo_file # combine multiple files 
 cat *.fasta > combo_fasta.fasta # combines all fasta files in a directory 
 
+
+####### so to make a multiple fasta from all files in a directory 
+cat *.fasta > combo_fasta.fasta
+
+### to make an aligned multiple sequence file 
+## upload file to online aligner tool and download / save output 
+
+## view MSA files with jalview or aliview
+
+
