e22368016ba37f6a72c502d80a14834a5b94080f
Dan Bruzzese <31770882+bruzecruise@users.noreply.github.com>
2025-08-26 11:38:54 -0400

    Create code_snippets.sh
    first commit woo

---
+++ code_snippets.sh
@@ -0,0 +1,8 @@
+command # what it does
+
+ls #pint contents of directory 
+cd # go to directory 
+cat #view file content on the screen 
+cat file1 file2 file3 file4 > combo_file # combine multiple files 
+cat *.fasta > combo_fasta.fasta # combines all fasta files in a directory 
+
